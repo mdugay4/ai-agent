440172ddebed97ac62bfb394ad7b29bf073e060b
Mark Dugay <mdugay4@gmail.com>
2024-04-04 22:04:32 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+<!-- create requirements.txt -->
+<!-- pip freeze > requirements.txt -->
+
+<!-- install requirements.txt -->
+<!-- pip install -r requirements.txt -->
+
+<!--
+    command to activate virtual environment (in powershell)
+    ./[virtual-environment-name]/../path-to-activate-script
+-->
+
+<!--
+    command to deactivate virtual environment
+    deactivate
+-->
+
+<!--
+    how to run main.py
+    1) make sure ai_venv is active
+    2) using powershell: python main.py
+-->
---
+++ main.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+import os
+import pandas as pd
+# from llama_index.query_engine import PandasQueryEngine
+from llama_index.core.query_engine import PandasQueryEngine
+from prompts import new_prompt, instruction_str
+from note_engine import note_engine
+from llama_index.tools import QueryEngineTool, ToolMetadata
+from llama_index.agent import ReActAgent
+from llama_index.llms import OpenAI
+
+load_dotenv()
+
+population_path = os.path.join("data", "world_pop_2023.csv")
+population_df = pd.read_csv(population_path)
+
+# print(population_df.head())
+population_query_engine = PandasQueryEngine(
+    df=population_df, verbose=True, instruction_str=instruction_str
+)
+
+population_query_engine.update_prompts({"pandas_prompt": new_prompt})
+population_query_engine.query("what is the population of the united states")
