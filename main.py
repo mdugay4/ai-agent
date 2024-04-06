b1ed6eb4c28f95804c683fa15a26517cb481794c
Mark Dugay <mdugay4@gmail.com>
2024-04-05 21:53:00 -0700

    tools, llm and agent

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import pandas as pd
 from llama_index.core.query_engine import PandasQueryEngine
 from prompts import new_prompt, instruction_str
 from note_engine import note_engine
-from llama_index.tools import QueryEngineTool, ToolMetadata
-from llama_index.agent import ReActAgent
-from llama_index.llms import OpenAI
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.agent import ReActAgent
+from llama_index.llms.openai import OpenAI
 
 load_dotenv()
 
@@ -20,4 +20,18 @@ population_query_engine = PandasQueryEngine(
 )
 
 population_query_engine.update_prompts({"pandas_prompt": new_prompt})
-population_query_engine.query("what is the population of the united states")
+# population_query_engine.query("what is the population of the united states")
+
+tools = [
+    note_engine,
+    QueryEngineTool(
+        query_engine=population_query_engine, 
+        metadata=ToolMetadata(
+            name="population_data",
+            description="this gives information at the world population and demographics"
+        ),
+    ),
+]
+
+llm = OpenAI(model="gpt-3.5-turbo-0613")
+agent = ReActAgent.from_tools(tools, llm=llm, verbose=True)
